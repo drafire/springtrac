9a9d1d0901d4750351d8f54a00f1bc1227e53802
drafire <hyljljlj@163.com>
2018-10-08 23:26:09 +0800

    使用注解式事务进行实现

--- src/main/java/com/example/springtrac/service/UserService.java
+++ src/main/java/com/example/springtrac/service/UserService.java
@@ -4,6 +4,9 @@ import com.example.springtrac.dao.UserDao;
 import com.example.springtrac.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 public class UserService {
@@ -11,7 +14,35 @@ public class UserService {
     @Autowired
     private UserDao userDao;
 
-    public User getUserInfo(Integer id) {
-        return userDao.getUser(id);
+    public User getUserInfo(Integer id) throws Exception {
+        User user = userDao.getUser(id);
+        if (user == null) {
+            throw new Exception("不存在该用户");
+        }
+        return user;
+    }
+
+    @Transactional
+    public void income(int incomeId, BigDecimal amout) throws Exception {
+        User user = getUserInfo(incomeId);
+        user.setAmout(user.getAmout().add(amout));
+        userDao.updateUser(user);
+        throw new Exception("转入失败");
+    }
+
+    @Transactional
+    public void outcome(int outcomeId, BigDecimal amout) throws Exception {
+        User user = getUserInfo(outcomeId);
+        if (user.getAmout().compareTo(amout) < 0) {
+            throw new Exception("账户余额不足");
+        }
+        user.setAmout(user.getAmout().subtract(amout));
+        user.setAmout(user.getAmout().subtract(amout));
+    }
+
+    @Transactional
+    public void doTransfer(int incomeId, int outcomeId, BigDecimal amout) throws Exception {
+        outcome(outcomeId, amout);
+        income(incomeId, amout);
     }
 }
--- src/test/java/com/example/springtrac/service/UserServiceTest.java
+++ src/test/java/com/example/springtrac/service/UserServiceTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
 //这里使用这两个注解就可以了
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -16,7 +18,12 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
-    public void getUserInfo() {
+    public void getUserInfo() throws Exception{
         User user = userService.getUserInfo(1);
+    }
+
+    @Test
+    public void doTransfer() throws Exception {
+        userService.doTransfer(1,2, new BigDecimal(30));
     }
 }
