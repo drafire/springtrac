0538421a5fc2bba630a2bb37dbcc8e8985049b76
drafire <hyljljlj@163.com>
2018-10-09 08:06:27 +0800

    区分事务的捕捉机制，事务不捕捉Exception，只捕捉RuntimeException和其子类的异常

--- src/main/java/com/example/springtrac/service/UserService.java
+++ src/main/java/com/example/springtrac/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.springtrac.dao.UserDao;
 import com.example.springtrac.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -22,15 +23,18 @@ public class UserService {
         return user;
     }
 
-    @Transactional
+    //@Transactional
     public void income(int incomeId, BigDecimal amout) throws Exception {
         User user = getUserInfo(incomeId);
         user.setAmout(user.getAmout().add(amout));
         userDao.updateUser(user);
-        throw new Exception("转入失败");
+        //这里抛出父异常类型，不能回滚
+        //throw new Exception("转入失败");
+        //事务只捕捉运行时的异常，这里抛出运行时异常，成功回滚
+        throw new RuntimeException("转入失败");
     }
 
-    @Transactional
+    //@Transactional
     public void outcome(int outcomeId, BigDecimal amout) throws Exception {
         User user = getUserInfo(outcomeId);
         if (user.getAmout().compareTo(amout) < 0) {
@@ -40,7 +44,8 @@ public class UserService {
         user.setAmout(user.getAmout().subtract(amout));
     }
 
-    @Transactional
+    //默认的事务传播行为，就是Propagation.REQUIRED
+    @Transactional(propagation = Propagation.REQUIRED)
     public void doTransfer(int incomeId, int outcomeId, BigDecimal amout) throws Exception {
         outcome(outcomeId, amout);
         income(incomeId, amout);
